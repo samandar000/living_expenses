1390ac2d26fcb5cb32f64c841a84e1a7dc99226e
Jalilov <javohirjalilov777@gmail.com>
2022-09-27 13:57:19 +0500

    added task

---
+++ main.py
@@ -0,0 +1,46 @@
+# Monthly expenses
+monthly_expenses = {
+    'rent': 1200,
+    'utilities': 300,
+    'transport': 450,
+    'food': 600,
+    'entertainment': 110,
+    'clothing': 220,
+    'health': 30,
+    'internet': 60,
+    'education': 200,
+    'other': 100
+}
+
+# Calculate the total expenses
+def total_expenses(monthly_expenses: dict) -> int:
+    """
+    Calculate the total expenses
+    Args:
+        monthly_expenses: dictionary of monthly expenses
+    Returns:
+        total_expenses: total expenses
+    """
+    return 
+
+# Find the least expensive expense
+def least_expensive(monthly_expenses: dict) -> str:
+    """
+    Find the least expensive expense
+    Args:
+        monthly_expenses: dictionary of monthly expenses
+    Returns:
+        least_expensive: least expensive expense
+    """
+    return
+
+# Find the most expensive expense
+def most_expensive(monthly_expenses: dict) -> str:
+    """
+    Find the most expensive expense
+    Args:
+        monthly_expenses: dictionary of monthly expenses
+    Returns:
+        most_expensive: most expensive expense
+    """
+    return
